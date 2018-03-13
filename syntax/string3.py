aa2e75b2e1e2c39c1467d27c4f39acdacd5471d6
egoing <egoing@gmail.com>
2018-03-13 11:43:48 +0900

    String 3 & Variable

---
+++ syntax/string3.py
@@ -0,0 +1,2 @@
+name = 'egoing'
+print('to '+name+'. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim apple veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '+name+' Duis aute irure dolor in reprehenderit apple computer in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui '+name+' officia deserunt mollit anim id est laborum.')
