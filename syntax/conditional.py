688eb77211b1302e9aa3140b4c35648e96b6991a
egoing <egoing@gmail.com>
2018-03-14 17:10:31 +0900

    Conditional2 & Logical operator

--- syntax/conditional.py
+++ syntax/conditional.py
@@ -7,10 +7,20 @@ if user_pwd == '111111':
 else:
     print('Who are you?')
 '''
+
+'''
 if user_id == 'egoing':
     if user_pwd == '111111':
         print('Hello master')
     else:
         print('Who are you?')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'egoing' and user_pwd == '111111':
+    print('Hello master')
+elif user_id == 'leezche' and user_pwd == '222':
+    print('Hello master')
 else:
     print('Who are you?')
