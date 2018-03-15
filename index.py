1afa66ffd8170ee51943e6f71d9f9c86075c04bc
egoing <egoing@gmail.com>
2018-03-16 00:13:33 +0900

    반복문을 이용해서 글목록 구현하기

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
 #!/usr/local/bin/python3
 print("Content-Type: text/html")
 print()
-import cgi
+import cgi, os
+
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+    
 form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
@@ -18,13 +24,10 @@ print('''<!doctype html>
 <body>
   <h1><a href="index.py">WEB</a></h1>
   <ol>
-    <li><a href="index.py?id=HTML">HTML</a></li>
-    <li><a href="index.py?id=CSS">CSS</a></li>
-    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
-    <li><a href="index.py?id=Python">Python</a></li>
+    {listStr}
   </ol>
   <h2>{title}</h2>
   <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId, desc=description))
+'''.format(title=pageId, desc=description, listStr=listStr))
