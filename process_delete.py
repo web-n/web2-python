324a04cc1db790e5a52d9e8cd7d5726160314e7f
egoing <egoing@gmail.com>
2018-03-16 13:24:16 +0900

    delete

---
+++ process_delete.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+import cgi, os
+form = cgi.FieldStorage()
+pageId = form["pageId"].value
+
+os.remove('data/'+pageId)
+
+#Redirection
+print("Location: index.py")
+print()
