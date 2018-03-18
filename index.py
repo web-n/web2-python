b0cb0d707d2ff3088ec141f10c47433916203833
egoing <egoing@gmail.com>
2018-03-18 21:36:22 +0900

    pip & sanitizer

--- index.py
+++ index.py
@@ -1,14 +1,15 @@
 #!/usr/local/bin/python3
 print("Content-Type: text/html")
 print()
-import cgi, os, view
+import cgi, os, view, html_sanitizer
+sanitizer = html_sanitizer.Sanitizer()
 
 form = cgi.FieldStorage()
 if 'id' in form:
-    pageId = form["id"].value
+    title = pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
-    description = description.replace('<', '&lt;')
-    description = description.replace('>', '&gt;')
+    title = sanitizer.sanitize(title)
+    description = sanitizer.sanitize(description)
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
     delete_action = '''
         <form action="process_delete.py" method="post">
@@ -17,7 +18,7 @@ if 'id' in form:
         </form>
     '''.format(pageId)
 else:
-    pageId = 'Welcome'
+    title = pageId = 'Welcome'
     description = 'Hello, web'
     update_link = ''
     delete_action = ''
@@ -40,7 +41,7 @@ print('''<!doctype html>
 </body>
 </html>
 '''.format(
-    title=pageId,
+    title=title,
     desc=description,
     listStr=view.getList(),
     update_link=update_link,
--- view.py
+++ view.py
@@ -1,8 +1,9 @@
-import os
+import os, html_sanitizer
 def getList():
+    sanitizer = html_sanitizer.Sanitizer()
     files = os.listdir('data')
     listStr = ''
     for item in files:
+        item = sanitizer.sanitize(item)
         listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
     return listStr
- 
